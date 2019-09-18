dd7eb511376329895cf27fe69e03f133d5040679
aiai949 <54714590+aiai949@users.noreply.github.com>
2019-09-18 14:16:43 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+       function changeImage(){
+           var image=document.getElementById('image');
+           if(image.src.match("me2.jpg")){
+             image.src="images/me4.jpg"; 
+           } else{
+               image.src="images/me2.jpg";
+           }
+       }
+    
+    
+    
